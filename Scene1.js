3b7c17be25065212df19a554389f86f6e90a3cc9
Anton Varonin <anton_voronin_2011@mail.ru>
2022-01-18 01:54:25 +0300

    fix anims and add tween.timelines

--- Scene1.js
+++ Scene1.js
@@ -20,60 +20,77 @@ class Scene1 extends Phaser.Scene {
 
     create () {
         this.diamond = this.add.sprite(config.width / 2, config.height / 2, "start");
-        
         this.diamond.setScale(0)
 
         this.anims.create({
             key: "start_anim", 
             frames: this.anims.generateFrameNumbers("start"),
-            frameRate: 8,
-            repeat: 2,
+            frameRate: 10,
+            repeat: -1,
 
         })
         this.anims.create({
             key: "middle_anim", 
             frames: this.anims.generateFrameNumbers("middle"),
-            frameRate: 8,
-            repeat: 1,
+            frameRate: 10,
+            repeat: -1,
         })
         this.anims.create({
             key: "finish_anim", 
             frames: this.anims.generateFrameNumbers("finish"),
-            frameRate: 8,
-            repeat: 1,
+            frameRate: 10,
+            repeat: -1,
 
         })
 
+        let diamond = this.diamond;
 
-        this.diamond.play("start_anim", 500, true);
+        let startDuration = 1500;
+        let middleDuration = 1500;
+        let finishDuration = 1500;
+
+        let timeline = this.tweens.createTimeline();
         
-        this.tweens.add({
+
+        timeline.add({
             targets: this.diamond,
             scale: 0.5,
-            duration: 1500,
             ease: 'linear',
-
-        })
-
-        this.tweens.add({
-            targets: this.diamond,
-            y: 100,
-            scale: 0,
-            duration: 3000,
-            ease: 'Back',   
-            delay: 2500
+            duration: startDuration,
+            onStart: function() {
+                diamond.play("start_anim")
+            }
         });
+
+        timeline.add({
+            targets: this.diamond,
+            scale: 0.5,
+            ease: 'linear',
+            duration: middleDuration,
+            onStart: function () {
+                diamond.play("middle_anim")
+            }
+        });
+
+        timeline.add({
+            targets: this.diamond,
+            y: "-=100",
+            scale: 0.09,
+            ease: 'linear',
+            duration: finishDuration,
+            onStart: function () {
+                diamond.play("finish_anim")
+            },
+            onComplete: function() {
+                diamond.play("middle_anim");
+                diamond.anims.stop();
+            }
+        });
+
+        timeline.play();
         
 
-        this.diamond.on('animationcomplete', ()=>{
-            this.diamond.play("middle_anim");
-            this.diamond.on('animationcomplete', ()=>{
-                this.diamond.play("finish_anim");
-                this.diamond.on('animationcomplete', ()=>{
-                    this.diamond.anims.stop();
-                });
-            });
-        });
+       
     }
 }
 
